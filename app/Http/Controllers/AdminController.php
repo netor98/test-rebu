200e4bedefcc8612b8cfde68e44bb32d0f7d884f
Nestor Padilla <pcu18021379@gmail.com>
2024-04-11 17:32:33 -0600

    asdasd

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -51,7 +51,7 @@ class AdminController extends Controller
         if ($request->hasfile('image')) {
             $file = $request->file('image');
             $file_name = time() . $file->getClientOriginalName();
-            $file->storeAs('uploads', $file_name);
+            $file->storeAs('public', $file_name);
             $insert->image = $file_name;
         }
 
