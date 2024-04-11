99a757d642873e8994908958ffca0f9022bdff4c
Nestor Padilla <pcu18021379@gmail.com>
2024-04-11 02:09:07 -0600

    asd

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,13 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    
+   
     return view('index');
 });
 
 Route::get('/create-symlink', function (){
-    symlink(storage_path('/app/public'), public_path('storage'));
+    symlink(storage_path('app/public'), public_path('storage'));
     echo "Symlink Created. Thanks";
 });
 
