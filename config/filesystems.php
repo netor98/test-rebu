8e44e9bfeffc983f851c62a6156ab7dd6d7635b1
Nestor Padilla <pcu18021379@gmail.com>
2024-04-11 18:14:33 -0600

    asdv

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path() . '/storage/uploads',
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
